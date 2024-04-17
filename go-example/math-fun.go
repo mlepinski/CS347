a66cf8dcc548dbe40e03e88082854a9412739235
mlepinski <mlepinski@gmail.com>
2024-04-17 13:08:46 -0500

    Update math-fun.go

--- go-example/math-fun.go
+++ go-example/math-fun.go
@@ -4,6 +4,7 @@ import (
        "fmt"        //package for Printing output
 )
 
+// Return Type goes at the end
 func add(x int, y int) int{
 	return x + y
 }
@@ -12,10 +13,13 @@ func add(x int, y int) int{
 func main(){
 	my_func := add
 
+	//Declare variables x and y
 	var x, y int
 
 	x = 2
 	y = 3
+
+	// This declares z
 	z := 5
 	
 	ans := my_func(x,y)
@@ -25,3 +29,4 @@ func main(){
 	fmt.Println("Hello!")
 	fmt.Println( ans )
 }
+
